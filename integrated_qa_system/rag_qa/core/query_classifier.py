0810b7e54d35eeb37d79c1306c8c3ea0e1814d46
白登超 <bacium_dc@163.com>
2026-05-09 23:11:18 +0800

    feat(rag_qa): 添加查询分类器训练功能
    - 实现了BERT模型的训练方法train_model
    - 添加了数据预处理方法preprocess_data和数据集创建方法create_dataset
    - 集成了scikit-learn的train_test_split进行数据分割
    - 添加了训练数据加载和验证逻辑
    - 创建了PyTorch Dataset类用于模型训练
    - 更新了初始化日志信息
    - 添加了训练数据集生成提示词模板文件

--- integrated_qa_system/rag_qa/core/query_classifier.py
+++ integrated_qa_system/rag_qa/core/query_classifier.py
@@ -51,9 +51,51 @@ class QueryClassifier:
             # 将模型移到指定设备
             self.model.to(self.device)
             # 记录初始化模型的日志
-            logger.info("初始化新 BERT 模型")
+            logger.info("初始化新 BERT 模型成功")
 
+    def train_model(self, data_file="../classify_data/model_generic_5000.json"):
+        if not os.path.exists(data_file):
+            logger.error(f"数据文件 {data_file} 不存在")
+            raise FileNotFoundError(f"数据集文件 {data_file} 不存在")
+        logger.info("训练数据加载成功")
+        with open(data_file, "r", encoding="utf-8") as f:
+            data = [json.loads(value) for value in f.readlines()]
+            # print(f"data======>{data}")
+        querys = [item["query"] for item in data]
+        labels = [item["label"] for item in data]
+        # print(f"querys====>{querys}")
+        # print(f"labels====>{labels}")
+        # print(f"len(labels)===>{len(labels)}")
+        train_querys, test_querys, train_labels, test_labels = train_test_split(querys, labels, test_size=0.2,
+                                                                                random_state=42)
+        # 数据预处理
+        train_encodings, train_labels = self.preprocess_data(train_querys, train_labels)
+        test_encodings, test_labels = self.preprocess_data(test_querys, test_labels)
+        train_dataset = self.create_dataset(train_encodings, train_labels)
+        test_dataset = self.create_dataset(test_encodings, test_labels)
+
+    def preprocess_data(self, querys, labels):
+        train_encodings = self.tokenizer(querys, truncation=True, padding=True, max_length=128, return_tensors="pt")
+        train_labels = [self.label_map[label] for label in labels]
+        # print(f"train_encodings===>{train_encodings['input_ids'].shape}")
+        # print(f"train_labels=======>{train_labels}")
+        return train_encodings, train_labels
+
+    def create_dataset(self):
+        class Dataset(torch.utils.data.Dataset):
+            def __init__(self, encodings, labels):
+                self.encodings = encodings
+                self.labels = labels
+
+            def __getitem__(self, idx):
+                item = {key: val[idx] for key, val in self.encodings.items()}
+                item['labels'] = torch.tensor(self.labels[idx])
+                return item
+
+            def __len__(self):
+                return len(self.labels)
 
 
 if __name__ == '__main__':
     query_classifier = QueryClassifier()
+    query_classifier.train_model()
